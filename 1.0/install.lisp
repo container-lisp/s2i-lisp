5394eb658c8f3563aa76d9ada58370e9b6c6e97d
Anthony Green <green@moxielogic.com>
2018-01-04 21:51:55 -0500

    Update to 2017-12-27

--- 1.0/install.lisp
+++ 1.0/install.lisp
@@ -1,4 +1,4 @@
-(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2017-10-23/distinfo.txt")
+(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2017-12-27/distinfo.txt")
 
 (load "quicklisp.lisp")
 
