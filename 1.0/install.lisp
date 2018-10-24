8de80bd4ab43d347f3bf1dfea70b91f47617ceef
Harald Judt <h.judt@gmx.at>
2018-10-24 14:17:37 +0200

    Bump quicklisp dist version to 2018-10-18

--- 1.0/install.lisp
+++ 1.0/install.lisp
@@ -1,4 +1,4 @@
-(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2018-08-31/distinfo.txt")
+(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2018-10-18/distinfo.txt")
 
 (load "quicklisp.lisp")
 
