f6d542fed44851cc00aeeaca33148adcfa291038
Anthony Green <green@moxielogic.com>
2019-07-17 13:11:20 -0400

    More testing

--- .travis/scan-container.sh
+++ .travis/scan-container.sh
@@ -5,7 +5,9 @@ set -x
 # -----------------------------------------------------------------------------
 # Use Clair
 
-sudo apt-get --yes remove postgresql\*
+# travis-ci comes with postgresql on, which clashes with the postgresql we're
+# about to fire up.
+sudo apt-get --yes remove postgresql\* > /dev/null 2>&1
 docker run -d --name db arminc/clair-db
 sleep 15 # wait for db to come up
 docker run -p 6060:6060 --link db:postgres -d --name clair arminc/clair-local-scan
@@ -13,6 +15,9 @@ sleep 1
 DOCKER_GATEWAY=$(docker network inspect bridge --format "{{range .IPAM.Config}}{{.Gateway}}{{end}}")
 wget -qO clair-scanner https://github.com/arminc/clair-scanner/releases/download/v12/clair-scanner_linux_amd64 && chmod +x clair-scanner
 ./clair-scanner --ip="$DOCKER_GATEWAY" $REPO
+# Tests
+./clair-scanner --ip="$DOCKER_GATEWAY" containerlisp/lisp-10-rhel7
+./clair-scanner --ip="$DOCKER_GATEWAY" atgreen/moxielogic-builder-f25
 
 # -----------------------------------------------------------------------------
 # Use the Anchore's inline scanner.
