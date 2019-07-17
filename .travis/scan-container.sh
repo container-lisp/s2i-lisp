8d85c268748d9236c4757a449a32457d86283e61
Anthony Green <green@moxielogic.com>
2019-07-17 12:59:52 -0400

    Fix clair scanning

--- .travis/scan-container.sh
+++ .travis/scan-container.sh
@@ -6,12 +6,13 @@ set -x
 # Use Clair
 
 sudo apt-get --yes remove postgresql\*
-wget https://raw.githubusercontent.com/singularityhub/stools/master/docker-compose.yml
-docker-compose up -d clair-db
-docker-compose up -d clair-scanner
-sleep 3
-docker exec -it clair-scanner sclair $REPO
-
+docker run -d --name db arminc/clair-db
+sleep 15 # wait for db to come up
+docker run -p 6060:6060 --link db:postgres -d --name clair arminc/clair-local-scan
+sleep 1
+DOCKER_GATEWAY=$(docker network inspect bridge --format "{{range .IPAM.Config}}{{.Gateway}}{{end}}")
+wget -qO clair-scanner https://github.com/arminc/clair-scanner/releases/download/v12/clair-scanner_linux_amd64 && chmod +x clair-scanner
+./clair-scanner --ip="$DOCKER_GATEWAY" $REPO
 
 # -----------------------------------------------------------------------------
 # Use the Anchore's inline scanner.
