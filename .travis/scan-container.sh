ad925cbf2ba1afae0982ec057d6534f3b02fad67
Anthony Green <green@moxielogic.com>
2019-07-19 18:37:06 -0400

    Fix error handling

--- .travis/scan-container.sh
+++ .travis/scan-container.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+RLGL_POLICY=https://github.com/atgreen/test-policy.git
+
+# -----------------------------------------------------------------------------
+# Error handling
+
 exit_on_error() {
     exit_code=$1
     last_command=${@:2}
@@ -9,10 +14,8 @@ exit_on_error() {
     fi
 }
 
-# enable !! command completion
+# enable "!!" command completion
 set -o history -o histexpand
-
-RLGL_POLICY=https://github.com/atgreen/test-policy.git
 
 # -----------------------------------------------------------------------------
 # Configure rlgl
@@ -20,15 +23,15 @@ RLGL_POLICY=https://github.com/atgreen/test-policy.git
 # Download and extract the client
 wget -qO - http://rl.gl/cli/rlgl-linux-amd64.tgz | \
     tar --strip-components=2 -xvzf - ./rlgl/rlgl
-exit_on_error $? !!
+exit_on_error $? "!!"
 
 # Log into the server
 ./rlgl login http://rl.gl
-exit_on_error $? !!
+exit_on_error $? "!!"
 
 # Generate a player ID for use during report evaluation
 ID=$(./rlgl start)
-exit_on_error $? !!
+exit_on_error $? "!!"
 
 # -----------------------------------------------------------------------------
 # Use Clair
@@ -43,13 +46,13 @@ sleep 1
 DOCKER_GATEWAY=$(docker network inspect bridge --format "{{range .IPAM.Config}}{{.Gateway}}{{end}}")
 
 wget -qO clair-scanner https://github.com/arminc/clair-scanner/releases/download/v12/clair-scanner_linux_amd64 && chmod +x clair-scanner
-exit_on_error $? !!
+exit_on_error $? "!!"
 
 ./clair-scanner --ip="$DOCKER_GATEWAY" -r clair-report.json $REPO
-exit_on_error $? !!
+exit_on_error $? "!!"
 
 ./rlgl e --id=$ID --policy=$RLGL_POLICY clair-report.json
-exit_on_error $? !!
+exit_on_error $? "!!"
 
 # -----------------------------------------------------------------------------
 # Use the Anchore's inline scanner.
@@ -57,14 +60,14 @@ exit_on_error $? !!
 curl -s https://ci-tools.anchore.io/inline_scan-v0.4.1 | bash -s -- -p -r $REPO
 if test -f anchore-reports/${REPO}*-vuln.json; then
     ./rlgl e --id=$ID --policy=$RLGL_POLICY anchore-reports/${REPO}*-vuln.json
-    exit_on_error $? !!
+    exit_on_error $? "!!"
 fi
 
 # Test to see output of bad container...
 curl -s https://ci-tools.anchore.io/inline_scan-v0.4.1 | bash -s -- -p -r containerlisp/lisp-10-centos7
 if test -f anchore-reports/lisp-10-centos7_latest-vuln.json; then
     ./rlgl e --id=$ID --policy=$RLGL_POLICY anchore-reports/lisp-10-centos7_latest-vuln.json
-    exit_on_error $? !!
+    exit_on_error $? "!!"
 fi
 
 # -----------------------------------------------------------------------------
@@ -72,7 +75,7 @@ fi
 
 if ! test -f microscanner; then
   wget https://get.aquasec.com/microscanner
-  exit_on_error $? !!
+  exit_on_error $? "!!"
   chmod +x microscanner
 fi  
 
@@ -84,7 +87,7 @@ RUN /microscanner $AQUACODE -c
 EOF
 
 docker build . -f Dockerfile.scan
-exit_on_error $? !!
+exit_on_error $? "!!"
 
 # Run rlgl here once Aqua has fixed their ubi8 scanner
 
@@ -93,4 +96,4 @@ exit_on_error $? !!
 # Summarize scans
 
 ./rlgl log --id=$ID
-exit_on_error $? !!
+exit_on_error $? "!!"
