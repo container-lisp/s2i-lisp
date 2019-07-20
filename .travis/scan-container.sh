07021c4556497fe05bb7f931022418f6cc011153
Anthony Green <green@moxielogic.com>
2019-07-19 20:09:47 -0400

    More hacks.

--- .travis/scan-container.sh
+++ .travis/scan-container.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
+set -e
+
 RLGL_POLICY=https://github.com/atgreen/test-policy.git
-
-# -----------------------------------------------------------------------------
-# Error handling
-
-exit_on_error() {
-    exit_code=$1
-    last_command=${@:2}
-    if [ $exit_code -ne 0 ]; then
-        >&2 echo "\"${last_command}\" command failed with exit code ${exit_code}."
-        exit $exit_code
-    fi
-}
-
-# enable "!!" command completion
-set -o history -o histexpand
 
 # -----------------------------------------------------------------------------
 # Configure rlgl
@@ -23,15 +10,12 @@ set -o history -o histexpand
 # Download and extract the client
 wget -qO - http://rl.gl/cli/rlgl-linux-amd64.tgz | \
     tar --strip-components=2 -xvzf - ./rlgl/rlgl
-exit_on_error $? "!!"
 
 # Log into the server
 ./rlgl login http://rl.gl
-exit_on_error $? "!!"
 
 # Generate a player ID for use during report evaluation
 ID=$(./rlgl start)
-exit_on_error $? "!!"
 
 # -----------------------------------------------------------------------------
 # Use Clair
@@ -46,22 +30,18 @@ sleep 1
 DOCKER_GATEWAY=$(docker network inspect bridge --format "{{range .IPAM.Config}}{{.Gateway}}{{end}}")
 
 wget -qO clair-scanner https://github.com/arminc/clair-scanner/releases/download/v12/clair-scanner_linux_amd64 && chmod +x clair-scanner
-exit_on_error $? "!!"
 
 ./clair-scanner --ip="$DOCKER_GATEWAY" -r clair-report.json $REPO
-exit_on_error $? "!!"
 
 ./rlgl e --id=$ID --policy=$RLGL_POLICY clair-report.json
-exit_on_error $? "!!"
 
 # -----------------------------------------------------------------------------
 # Use the Anchore's inline scanner.
 
-curl -s https://ci-tools.anchore.io/inline_scan-v0.4.1 | bash -s -- -p -r $REPO
-if test -f anchore-reports/${REPO}*-vuln.json; then
-    ./rlgl e --id=$ID --policy=$RLGL_POLICY anchore-reports/${REPO}*-vuln.json
-    exit_on_error $? "!!"
-fi
+#curl -s https://ci-tools.anchore.io/inline_scan-v0.4.1 | bash -s -- -p -r $REPO
+#if test -f anchore-reports/${REPO}*-vuln.json; then
+#    ./rlgl e --id=$ID --policy=$RLGL_POLICY anchore-reports/${REPO}*-vuln.json
+#fi
 
 # Test to see output of bad container...
 # We have to resort to this hack, as travis may timeout on long-running silent processes...
@@ -71,7 +51,6 @@ while kill -0 $BASHPID 2>/dev/null; do echo "Waiting for the anchore scanner"; s
 
 if test -f anchore-reports/lisp-10-centos7_latest-vuln.json; then
     ./rlgl e --id=$ID --policy=$RLGL_POLICY anchore-reports/lisp-10-centos7_latest-vuln.json
-    exit_on_error $? "!!"
 fi
 
 # -----------------------------------------------------------------------------
@@ -79,7 +58,6 @@ fi
 
 if ! test -f microscanner; then
   wget https://get.aquasec.com/microscanner
-  exit_on_error $? "!!"
   chmod +x microscanner
 fi  
 
@@ -91,7 +69,6 @@ RUN /microscanner $AQUACODE -c
 EOF
 
 docker build . -f Dockerfile.scan
-exit_on_error $? "!!"
 
 # Run rlgl here once Aqua has fixed their ubi8 scanner
 
@@ -100,4 +77,3 @@ exit_on_error $? "!!"
 # Summarize scans
 
 ./rlgl log --id=$ID
-exit_on_error $? "!!"
