ff1f38664712b15162db58d5952e70843679ee3c
Anthony Green <green@moxielogic.com>
2019-12-02 22:00:59 -0500

    Remove junk

--- .travis/scan-container.sh
+++ .travis/scan-container.sh
@@ -65,7 +65,7 @@ wget -qO clair-scanner https://github.com/arminc/clair-scanner/releases/download
 # Use Anchore's inline scanner...
 # -----------------------------------------------------------------------------
 
-curl -s https://ci-tools.anchore.io/inline_scan-v0.5.0 | bash -s -- -t 3300 -r localhost/$REPO
+curl -s https://ci-tools.anchore.io/inline_scan-v0.5.0 | bash -s -- -t 3300 -r $REPO
 ls -lRt
 if test -f anchore-reports/*-vuln.json; then 
     ./rlgl e --id=$ID --policy=$RLGL_POLICY anchore-reports/*-vuln.json
