565b2a536e899f64ed236204310672dc8e1cf195
Harald Judt <h.judt@gmx.at>
2020-02-22 00:04:18 +0100

    Bump quicklisp dist version to 2020-02-18

--- 1.0/root/opt/app-root/install.lisp
+++ 1.0/root/opt/app-root/install.lisp
@@ -1,4 +1,4 @@
-(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2019-12-27/distinfo.txt")
+(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2020-02-18/distinfo.txt")
 
 (load "quicklisp.lisp")
 
