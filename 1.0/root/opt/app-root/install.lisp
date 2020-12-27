5e6994b486a8f9102ae48c43f72f34da7dd5082d
Anthony Green <green@moxielogic.com>
2020-12-27 10:00:26 -0500

    Bump quicklisp dist version to 2020-12-20

--- 1.0/root/opt/app-root/install.lisp
+++ 1.0/root/opt/app-root/install.lisp
@@ -1,4 +1,4 @@
-(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2020-10-16/distinfo.txt")
+(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2020-12-20/distinfo.txt")
 
 (load "quicklisp.lisp")
 
