917c97e98316fe093287b61da193fe72bb0cb537
Anthony Green <green@moxielogic.com>
2021-04-19 22:45:16 -0400

    Bump quicklisp dist version to 2021-04-11

--- 1.0/root/opt/app-root/install.lisp
+++ 1.0/root/opt/app-root/install.lisp
@@ -1,4 +1,4 @@
-(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2021-02-28/distinfo.txt")
+(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2021-04-11/distinfo.txt")
 
 (load "quicklisp.lisp")
 
