a49ee30780b824099484ebea970de38436d93065
Anthony Green <green@moxielogic.com>
2022-08-28 11:01:38 -0400

    Fix quote problem.

--- hack/update-base.sh
+++ hack/update-base.sh
@@ -14,13 +14,9 @@ which grype &>/dev/null || die "This script requires grype."
 VULNS=$(grype -q -o table --only-fixed quay.io/containerlisp/lisp-10-ubi8:latest | awk '{printf("%25s\t%s\n", $1, $5)}')
 
 LISP_BASE_BUILD_DATE=$(date)
-sed -i "s/LISP_BASE_BUILD_DATE=.*/LISP_BASE_BUILD_DATE=$LISP_BASE_BUILD_DATE/g" ../1.0/Dockerfile
+sed -i "s/LISP_BASE_BUILD_DATE=.*/LISP_BASE_BUILD_DATE=\"$LISP_BASE_BUILD_DATE\"/g" ../1.0/Dockerfile
 
 echo "Adding git commit"
 git add ../1.0/Dockerfile && git commit -m "Fix vulnerabilities in the base image.
 
 $VULNS"
-
-
-
-
