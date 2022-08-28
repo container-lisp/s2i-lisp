2df255b8482452aa1c99ea465148adb736d80c9a
Anthony Green <green@moxielogic.com>
2022-08-28 11:23:38 -0400

    Make update-base.sh a little smarter.

--- hack/update-base.sh
+++ hack/update-base.sh
@@ -11,12 +11,18 @@ die() {
 
 which grype &>/dev/null || die "This script requires grype."
 
-VULNS=$(grype -q -o table --only-fixed quay.io/containerlisp/lisp-10-ubi8:latest | awk '{printf("%25s\t%s\n", $1, $5)}')
+echo "Scanning latest image for fixed vulnerabilities..."
+podman rmi quay.io/containerlisp/lisp-10-ubi8:latest > /dev/null
+VULNS=$(grype -q -o table --only-fixed quay.io/containerlisp/lisp-10-ubi8:latest | grep -i rpm | awk '{printf("%25s\t%s\n", $1, $5)}')
 
-LISP_BASE_BUILD_DATE=$(date)
-sed -i "s/LISP_BASE_BUILD_DATE=.*/LISP_BASE_BUILD_DATE=\"$LISP_BASE_BUILD_DATE\"/g" ../1.0/Dockerfile
-
-echo "Adding git commit"
-git add ../1.0/Dockerfile && git commit -m "Fix vulnerabilities in the base image.
+if ! test -z $VULNS; then
+    LISP_BASE_BUILD_DATE=$(date)
+    sed -i "s/LISP_BASE_BUILD_DATE=.*/LISP_BASE_BUILD_DATE=\"$LISP_BASE_BUILD_DATE\"/g" ../1.0/Dockerfile
+    echo "Adding git commit"
+    git add ../1.0/Dockerfile && git commit -m "Fix vulnerabilities in the base image.
 
 $VULNS"
+else
+    echo "Nothing to do"
+fi
+
