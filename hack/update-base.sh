3b194535a5d14985587fff3e69a19832ddb45ab8
Anthony Green <green@moxielogic.com>
2022-09-07 10:24:20 -0400

    Fix update-base.sh script in the absense of vulns

--- hack/update-base.sh
+++ hack/update-base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 # Update the LISP_BASE_BUILD_DATE environment variable in order to
 # trigger a rebuild of the image.  Trigger this when there are CVE
 # errata from the base OS image to be applied.
@@ -15,7 +15,7 @@ echo "Scanning latest image for fixed vulnerabilities..."
 podman rmi quay.io/containerlisp/lisp-10-ubi8:latest > /dev/null
 VULNS=$(grype -q -o table --only-fixed quay.io/containerlisp/lisp-10-ubi8:latest | grep -i rpm | awk '{printf("%25s\t%s\n", $1, $5)}')
 
-if ! test -z $VULNS; then
+if ! test -z "$VULNS"; then
     LISP_BASE_BUILD_DATE=$(date)
     sed -i "s/LISP_BASE_BUILD_DATE=.*/LISP_BASE_BUILD_DATE=\"$LISP_BASE_BUILD_DATE\"/g" ../1.0/Dockerfile
     echo "Adding git commit"
